6deda9b9c8b579304de2a9c42e557ef2627f9487
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-09-15 17:46:01 +0900

    new day

--- Topics/Hierarchy of exceptions/RuntimeException/src/Main.kt
+++ Topics/Hierarchy of exceptions/RuntimeException/src/Main.kt
@@ -1,9 +1,9 @@
 fun main() {
     try {
-    // write your code here, do not touch the lines above
-    val num = 2 / 0
-    // do not touch the lines below    
-    } catch(e: RuntimeException) {
+        // write your code here, do not touch the lines above
+        val num = 2 / 0
+        // do not touch the lines below
+    } catch (e: RuntimeException) {
         println("Well")
     } catch (e: Exception) {
         println("Wrong")
